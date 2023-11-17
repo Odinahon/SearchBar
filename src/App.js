2b819827e9cbba5005095c08e5d58760ba5f46cf
Odina Saydahmedova <odinasayid@gmail.com>
2023-11-17 10:08:41 -0800

    SearchBar functionality with expanded search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
+
+var data = require("./MOCK_DATA.json");
+
 function App() {
+  const [value, setValue] = useState('');
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  }
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    //our api to fetch search result
+    console.log('search', searchTerm);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search</h1>
+      <div className='search-container'>
+        <div className='search-inner'>
+          <input type='text' value={value} onChange={onChange}></input>
+          <button onClick={ () => onSearch(value)}> Search</button>
+        </div>
+        <div className='dropdown'>
+          {data.filter(item => {
+            const searchItem = value.toLowerCase();
+            const full_name = item.full_name.toLowerCase();
+            return searchItem && full_name.startsWith(searchItem) && full_name !== searchItem;
+          }).slice(0,10).map((item) => (
+            <div onClick = {() => onSearch(item.full_name)} className='dropdown-row' key={item.full_name}>
+              {item.full_name}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
